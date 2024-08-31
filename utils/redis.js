03cc81be821adde52fc037df07cafcc9646bc24b
“Jumoke1” <kazeemjumoke12@gmail.com>
2024-08-31 10:08:26 -0700

    redis

---
+++ utils/redis.js
@@ -0,0 +1,61 @@
+import redis from 'redis';
+
+class RedisClient {
+    constructor() {
+        // Create a Redis client
+        this.client = redis.createClient();
+
+        // Handle Redis client errors
+        this.client.on('error', (err) => {
+            console.error('Redis Client Error:', err);
+        });
+    }
+
+    // Check if the Redis client is connected
+    isAlive() {
+        return this.client.connected;
+    }
+
+    // Get the value associated with a key in Redis
+    async get(key) {
+        return new Promise((resolve, reject) => {
+            this.client.get(key, (err, value) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(value);
+                }
+            });
+        });
+    }
+
+    // Set a key-value pair in Redis with an expiration time (in seconds)
+    async set(key, value, duration) {
+        return new Promise((resolve, reject) => {
+            this.client.setex(key, duration, value, (err, reply) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(reply);
+                }
+            });
+        });
+    }
+
+    // Delete a key-value pair from Redis
+    async del(key) {
+        return new Promise((resolve, reject) => {
+            this.client.del(key, (err, reply) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(reply);
+                }
+            });
+        });
+    }
+}
+
+// Create and export an instance of RedisClient
+const redisClient = new RedisClient();
+export default redisClient;
