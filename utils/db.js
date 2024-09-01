d0bb75b379076e75ee3d82b7e485bebd8630c7dd
“Jumoke1” <kazeemjumoke12@gmail.com>
2024-09-01 02:11:03 -0700

    db

--- utils/db.js
+++ utils/db.js
@@ -1,33 +1,33 @@
 import { MongoClient } from 'mongodb';
-import {promisify } from 'util';
+// import { promisify } from 'util';
 
 class DBClient {
-    constructor() {
-	const host = process.env.DB_HOST || 'localhost'
-	const port = process.env.DB_PORT || 27017;
-	const database = process.env.DB_DATABASE || 'files_manager';
-	this.uri = `mongodb://${host}:${port}`;
-	this.dbName = database;
-	this.client = new MongoClient(this.uri, { useUnifiedTopology: true });
-	
-	this.client.connect().then(() => {
-	    this.db = this.client.db(this.dbName);
-	}).catch((err) => {
-	    console.error('MongoDB connection error:', err);
-	});
-    }
+  constructor() {
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || 27017;
+    const database = process.env.DB_DATABASE || 'files_manager';
+    this.uri = `mongodb://${host}:${port}`;
+    this.dbName = database;
+    this.client = new MongoClient(this.uri, { useUnifiedTopology: true });
 
-    isAlive() {
-	return this.client.topology.isConnected();
-    }
+    this.client.connect().then(() => {
+      this.db = this.client.db(this.dbName);
+    }).catch((err) => {
+      console.error('MongoDB connection error:', err);
+    });
+  }
 
-    async nbUsers() {
-	return this.db.collection('users').countDocuments();
-    }
+  isAlive() {
+    return this.client.topology.isConnected();
+  }
 
-    async nbFiles() {
-	return this.db.collection('files').countDocuments();
-    }
+  async nbUsers() {
+    return this.db.collection('users').countDocuments();
+  }
+
+  async nbFiles() {
+    return this.db.collection('files').countDocuments();
+  }
 }
 
 const dbClient = new DBClient();
