358b01437b0e73eb816f2d7c05cdf4667b03d459
“Jumoke1” <kazeemjumoke12@gmail.com>
2024-09-02 22:48:50 -0700

    server

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const AppController = require('../controllers/AppController');
-const express = require('express');
 
+const router = express.Router();
 router.get('/status', AppController.getStatus);
 router.get('/status', AppController.getStats);
 
