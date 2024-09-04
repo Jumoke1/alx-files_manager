6c4129e0da4c711f3a25ce003168d59dcbb1809a
“Jumoke1” <kazeemjumoke12@gmail.com>
2024-09-03 18:12:47 -0700

    routes

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const AppController = require('../controllers/AppController');
+const UserController = require('../controllers/UsersController');
 
 const router = express.Router();
+
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
+router.post('/users', UsersController.postNew);
 
 module.exports = router;
